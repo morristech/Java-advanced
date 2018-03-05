77921a59e1a32833bf74cb0e97cb9279cd6cdea8
Wang Shouheng <shouheng2015@gmail.com>
2018-03-05 15:26:48 +0800

    atest

--- Java-advanced/resources/java-advanced/src/main/java/me/shouheng/java8features/Java8InterfaceExample.java
+++ Java-advanced/resources/java-advanced/src/main/java/me/shouheng/java8features/Java8InterfaceExample.java
@@ -1,16 +1,22 @@
 package me.shouheng.java8features;
 
-public class Java8DefaultInterfaceExample {
+public class Java8InterfaceExample {
 
     public static void main(String ...args) {
         MyInterface1 myInterface1 = new MyInterface1();
         MyInterface2 myInterface2 = new MyInterface2();
+
         /**
          * use default implementation */
         myInterface1.sayHello();
+
         /**
          * use override implementation */
         myInterface2.sayHello();
+
+        /**
+         * use static method to create an instance */
+        DefaultInterface.create().sayHello();
     }
 
     private static class MyInterface1 implements DefaultInterface { }
@@ -24,6 +30,15 @@ public class Java8DefaultInterfaceExample {
     }
 
     private interface DefaultInterface {
+
+        static DefaultInterface create() {
+            return new DefaultInterface() {
+                @Override
+                public void sayHello() {
+                    System.out.println("Hello from static create method!");
+                }
+            };
+        }
 
         default void sayHello() {
             System.out.println("Hello default interface method!");
---
+++ Java-advanced/resources/java-advanced/src/main/java/me/shouheng/java8features/Java8TimeExample.java
@@ -0,0 +1,7 @@
+package me.shouheng.java8features;
+
+public class Java8TimeExample {
+    public static void main(String...args) {
+
+    }
+}
